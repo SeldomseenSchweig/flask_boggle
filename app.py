f49d0141a3f9bfcda1934a943d0e0a7e8b04a5de
SeldomseenSchweig <jamesschweig@gmail.com>
2022-07-13 21:24:13 -0700

    Added timer when page starts, added restart button, ISSUE: Session variables not persisting after refresh

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from crypt import methods
 from boggle import Boggle
 from flask import Flask, request, render_template, session, flash, jsonify, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hayduke"
@@ -16,7 +17,7 @@ def display_board():
     session['game_board'] = board
     session['number_of_games'] = 0
     session['hi_score'] = 0
-    return render_template('boggle_home.html', board=board, hi_score=session['hi_score'], number_of_games = session['number_of_games'])
+    return render_template('boggle_home.html', board=board, hi_score =session['hi_score'])
 
 
 @app.route("/check-word")
@@ -28,22 +29,24 @@ def check_word():
     result = jsonify(result)
     return result
 
-@app.route('/score')
+@app.route('/score', methods=['POST'])
 def keep_score():
-    """Supposed to update score and number of games """
+    """Supposed to update hi_score and number of games, un-implimented """
     hi_score = session['hi_score']
     session['number_of_games'] = session['number_of_games'] + 1
     number_of_games = session['number_of_games']
-    score = request.args['score']
-    score = int(score)
+    score = request.get_data('points')
+    score = json.loads(score)
+    score = score['points']
+    print(score, hi_score)
     if score > hi_score:
         hi_score = score
         session['hi_score'] = hi_score
-        new_score_and_game_number = {'hi_score': hi_score, 'number_of_games': number_of_games}
-        return new_score_and_game_number
+        new_high_score = {'hi_score': hi_score}
+        return new_high_score
     else:
-        new_score_and_game_number = {'hi_score': hi_score, 'number_of_games': number_of_games}
-        return jsonify(new_score_and_game_number)
+        new_high_score = {'hi_score': hi_score}
+        return new_high_score
 
 
 
--- static/boggle.js
+++ static/boggle.js
@@ -10,7 +10,7 @@ let gameOver = false
 async function sendWordToServer(input) {
     // Sends word to server to check if it's on the board and in the text
 
-    const response = await axios.get ("/check-word", { params: { word: input }});    
+    const response = await axios.get("/check-word", { params: { word: input }});    
     wordLength = input.length;
     let data = response.data.result;
     if (alreadyGuessed(input) ) {
@@ -24,8 +24,9 @@ async function sendScoreToServer(score){
     //Supposed to send the score to the backend 
     // to update high score and trigger a count of the number of games played
 
-
-    const response = await axios.get ("/score", { params: { score: score }});
+    const response = await axios.post ("/score", { points: score });
+    let data = response.data.hi_score;
+    $('#highScore').text(data);
 
 }
 
@@ -33,7 +34,7 @@ async function sendScoreToServer(score){
 // starts timer for the game. Timer doesn't sstop sometimes(Need to fix)
   $(document).on('click','#button', (e) => {
       e.preventDefault();
-      myInterval = setInterval(timer, 1000); 
+      
       input = $('#word').val();
       if (input.length === 0 ){
           alert("Enter a word please");
@@ -41,6 +42,13 @@ async function sendScoreToServer(score){
       sendWordToServer(input);
       $('#word').val('')
   });
+  $(document).on('click','#restart', (e) => {
+      e.preventDefault();
+
+   
+});
+
+  myInterval = setInterval(timer, 1000); 
 
 
 
@@ -49,11 +57,11 @@ async function sendScoreToServer(score){
   function updateScore(data, wordLength, input) {
       if (data === "ok"){
         $('#score').text(score = score + wordLength)
-        $('#correct').append(`<td> ${input}<td/>`)
+        $('#correct').append(`<td> ${input}</td>`)
         $('#message').text('is valid and on the board.')
 
       }else if ( data === "not-on-board" ){
-        $('#wrong').append(`<td> ${input}<td/>`)
+        $('#wrong').append(`<td> ${input}</td>`)
         $('#message').text(' is not on the board.')
       }else{
         $('#message').text(' is not even a word!')
@@ -78,12 +86,14 @@ async function sendScoreToServer(score){
 // Timer function, something is wrong, it doesn't 
 // stop nor does it send data about score to back end
     function timer() { 
-        if (time <= 6){
+        if (time <= 5){
             $('#timer').text(time++)
         }else{
             clearInterval(myInterval)
             $('#button').prop('disabled', true)
             sendScoreToServer(score)
+            $('#form').append(`<button id="restart"> Restart </button>`)
+
         }
     }
 
